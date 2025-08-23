f56592bf3129c8beb35636d5930cde53630525c5
mtrifonov <mtrifonov.design@gmail.com>
2025-08-23 19:35:41 +0200

    listed out as a viable folder

---
+++ out/_next/static/chunks/bed6dd00a58dd846.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/57d4aeb3276f1842.js",
+  "static/chunks/turbopack-124d8614613838e1.js"
+])
---
+++ out/_next/static/mTddCaY5WJsfxfUCxwAqb/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Ftools\u002F[toolId]","\u002Ftools\u002F[toolId]\u002Fopengraph-image.png"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
